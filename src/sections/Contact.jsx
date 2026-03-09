1066d79b6ab87fcde37e2d3838b8cd1cc50f9fe5
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-03-09 22:59:53 +0530

    update width state whenever the window is resized

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 function useWindowWidth() {
     const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 1200);
 
-    
+    useEffect(() => {
+        const handle = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handle);
+    })
 }
